11962aa812a566efbb5782876692ebb1df574fa9
Lukas Grützmacher <github@lg2.de>
2020-12-09 17:21:22 +0100

    Creating project using NuGet reference to DependentLibrary fails at runtime. The assembly BaseLibrary cannot be found. Ok, it is not there.

---
+++ ConsoleWithNugetReference/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DependentLibrary;
+
+namespace ConsoleWithNugetReference
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World with NuGet!");
+
+            var dependentClass = new DependentClass();
+            dependentClass.Method();
+        }
+    }
+}
