e2dbc8c0265217164f4022347f1c7e65125e4c7a
Lukas Grützmacher <github@lg2.de>
2020-12-09 17:34:37 +0100

    Explicitly loading assembly "solves" the problem.

--- ConsoleWithNugetReference/Program.cs
+++ ConsoleWithNugetReference/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Loader;
 using DependentLibrary;
 
 namespace ConsoleWithNugetReference
@@ -8,6 +10,9 @@ namespace ConsoleWithNugetReference
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World with NuGet!");
+
+            var location = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            AssemblyLoadContext.Default.LoadFromAssemblyPath(Path.Combine(location!, "BaseLibrary.dll"));
 
             var dependentClass = new DependentClass();
             dependentClass.Method();
